60fb3686ef7c7b27a0b6a8f23eb7670502a334d6
Paulo Lourenço <paulohll14@gmail.com>
2019-10-19 14:22:27 -0300

    Services separated by controllers

---
+++ src/controllers/InfocrimController.kt
@@ -0,0 +1,62 @@
+package com.phlourenco.controllers
+
+import com.phlourenco.definitions.InfocrimResponse
+import io.ktor.application.call
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import org.openqa.selenium.By
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.CapabilityType
+import org.openqa.selenium.remote.DesiredCapabilities
+
+fun Route.infocrimController() {
+
+    get("/infocrim") {
+        var driver = ChromeDriver()
+        val diretorio = System.getProperty("user.dir")+"/downloads"
+        val chromePref = HashMap<String, Any>()
+        chromePref.put("profile.default_content_settings.popups", 0)
+        chromePref.put("download.default_directory", diretorio)
+        chromePref.put("download.prompt_for_download" ,false)
+        chromePref.put("download.directory_upgrade",true)
+        chromePref.put("plugins.always_open_pdf_externally", true)
+        val options = ChromeOptions()
+        options.setExperimentalOption("prefs", chromePref)
+        options.addArguments("--kiosk-printing")
+        options.addArguments("--print-to-pdf")
+        options.addArguments("disable-popup-blocking")
+        val cap = DesiredCapabilities.chrome()
+        cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true)
+        cap.setCapability(ChromeOptions.CAPABILITY, options)
+
+        driver = ChromeDriver(cap)
+        login(driver)
+        driver.navigate().to("http://ec2-18-231-116-58.sa-east-1.compute.amazonaws.com/infocrim/login.html")
+        waitUntilPageIsReady(driver)
+        driver.findElementByName("cd_usuario").sendKeys("fiap")
+        driver.findElementByName("cd_senha").sendKeys("mpsp")
+
+        driver.findElementByName("cd_senha").findElement(By.xpath("./..")).findElement(By.xpath("./..")).findElements(
+            By.tagName("td")).last().findElements(By.tagName("a")).first().click()
+        waitUntilPageIsReady(driver)
+        driver.findElementById("enviar").click()
+        waitUntilPageIsReady(driver)
+
+        driver.findElementsByClassName("linhaDet").forEach {
+            if (!it.findElements(By.tagName("a")).isNullOrEmpty()) {
+                it.findElement(By.tagName("a")).click()
+                waitUntilPageIsReady(driver)
+
+                stringToPdfBase64(driver.pageSource)?.let {
+                    val response = InfocrimResponse(it)
+                    call.respond(response)
+                }
+
+                driver.close()
+                return@get
+            }
+        }
+    }
+}
---
+++ src/controllers/SivecController.kt
@@ -0,0 +1,55 @@
+package com.phlourenco.controllers
+
+import com.phlourenco.definitions.Address
+import com.phlourenco.definitions.SivecRequest
+import com.phlourenco.definitions.SivecResponse
+import io.ktor.application.call
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.routing.post
+import org.openqa.selenium.chrome.ChromeDriver
+
+fun Route.sivecController() {
+    post("/sivec") {
+        val req = this.call.receive<SivecRequest>()
+        val driver = ChromeDriver()
+        login(driver)
+        driver.navigate().to("http://ec2-18-231-116-58.sa-east-1.compute.amazonaws.com/sivec/login.html")
+        waitUntilPageIsReady(driver)
+        driver.findElementByName("nomeusuario").sendKeys("fiap")
+        driver.findElementByName("senhausuario").sendKeys("mpsp")
+        driver.findElementByName("Acessar").click()
+        moveTo(driver,"Pesquisa", true)
+        moveTo(driver,"Por Réu", true)
+        val type = validateSivecSearchType(req.searchType)
+        moveTo(driver,type, true)
+        waitUntilPageIsReady(driver)
+        driver.findElementById(getIdTextFieldForType(req.searchType)).sendKeys(req.term)
+        driver.findElementById("procura").click()
+        waitUntilPageIsReady(driver)
+        moveTo(driver,"1.157.644",true)
+        val results = driver.findElementsByClassName("textotab")
+        val response = SivecResponse(results[8].text,
+            results[9].text,
+            results[10].text,
+            results[11].text,
+            results[14].text,
+            results[15].text,
+            results[16].text,
+            results[17].text,
+            convertToBolean(results[18].text),
+            results[20].text,
+            results[22].text,
+            results[24].text,
+            results[26].text,
+            results[27].text,
+            results[25].text,
+            results[23].text,
+            Address(results[28].text, results[29].text)
+        )
+
+        call.respond(response)
+        driver.close()
+    }
+}
